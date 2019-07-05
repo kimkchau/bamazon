0a26043c7680aef3f61a9a746ecd9056b7e54661
Kim C <kimkly.chau@gmail.com>
2019-07-05 14:52:03 -0700

    updated .js file with functional code

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -19,74 +19,106 @@ var connection = mysql.createConnection({
 
 // connect to the mysql server and sql database
 connection.connect(function (err) {
-    console.log("Connected as id: " + connection.threadId);
-    
+    // console.log("Connected as id: " + connection.threadId);
+
     if (err) throw err;
-    // run the start function after the connection is made to promp the user
-    
-    connection.query("SELECT  FROM products", function (err, res) {
-        console.log("selected table query")
+
+    // Displays the products table from the bamazon_DB
+    connection.query("SELECT * FROM products", function (err, res) {
+        // console.log("selected table query")
         if (err) throw err;
         console.table(res);
+        // run the start function after the connection is made to prompt the user
+        start();
     });
 
 
-
-    start();
 });
-
-// console.table(products);
 
 // function which prompts the user for what item they would like to buy
 function start() {
     inquirer
-        .prompt({
-            name: "idBuy",
-            type: "list",
-            message: "Please select the ID of the product you would like to buy?",
-            choices: [
-                "1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "exit"
-            ]
-        })
-        .then(function (answer) {
-            switch (answer.idBuy) {
-                case "1":
-                    console.log("selected id 1")
-                    buyUnit1();
-                    break;
-
-                case "2":
-                    console.log("selected id 2");
-                    buyUnit2();
-                    break;
-
-                case "exit":
-                    console.log("selected exit");
-                    connection.end();
-                    break;
-
+        .prompt([
+            {
+                name: "idBuy",
+                type: "input",
+                message: "Please select the ID of the product you would like to buy?",
+                // choices: [
+                //     "1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "exit"
+                // ]
+            },
+            {
+                name: "buy",
+                type: "input",
+                message: `How many ${res[].product_name}would you like to buy?`
             }
+        ])
+        .then(function (answer) {
 
-            // function buyUnit1() {
-            //     inquirer
-            //     .prompt({
-            //         name: "buy",
-            //         type: "input",
-            //         message: "How many would you like to buy?"
-            //     })
-            //     .then(function(answer) {
-            //         var query = "SELECT "
-            //     })
-            // }
+            parseInt(answer.idBuy);
+            parseInt(answer.buy);
+
+            var index = answer.idBuy - 1;
+            connection.query("SELECT stock_quantity, price FROM products", function (err, res) {
+                if (err) throw err;
+                console.log(res[index].stock_quantity);
+                console.log(answer.buy);
+
+                if (answer.buy > res[index].stock_quantity) {
+                    console.log("\nInsufficient quantity, please select a lower quantity\n");
+                    start();
+                }
+
+
+                else {
+                    var qtyUpdate = res[index].stock_quantity - answer.buy;
+                    console.log(qtyUpdate);
+                    var total = res[index].price * answer.buy;
+                    console.log(res[index].price);
+                    console.log(typeof (res[index].price));
+
+                    console.log("\nYour Order has been placed.  Your Total is $ " + total.toFixed(2) + "\n");
+
+                    connection.query("UPDATE products SET ? WHERE ?",
+                        [
+                            {
+                                stock_quantity: qtyUpdate
+                            },
+                            {
+                                id: answer.idbuy
+                            }
+                        ],
+                        function (err, res) {
+                            if (err) throw err;
+                            console.log(res.affectedRows + " products updated!\n");;
+                        })
+                };
+            });
         });
-
-
-
-
-
 }
 
 
-// TODO: first display all of the items available for sale. Include the ids, names, and prices of products for sale.
-// TODO: prompt users with two messages: (first) Ask them the ID of the product they would like to buy (second) Ask how many units of the product they would like to buy
-// TODO: 
+
+    // When there are sufficient quantity, the order will process
+    // function placeOrder() {
+    // var index = answer.idBuy - 1;
+    // var qtyUpdate = res[index].stock_quantity - answer.buy;
+    // console.log(qtyUpdate);
+    // var total = res[index].price * answer.buy;
+    // console.log(total);
+
+    // connection.query("UPDATE products SET ? WHERE ?", [{
+    //     stock_quantity: qtyUpdate
+    // },
+    // {
+    //     id: answer.idbuy
+    // }],
+    //     function (err, res) {
+    //         if (err) throw err;
+
+    //         console.log(typeof (answer.buy));
+
+    //         console.log(res.affectedRows + `Your order has been placed.  Your total is ${total}`);
+    //         start();
+    //     });
+    // }
